ee43fc8cf033832a41f93d5616586f647fcef455
MattClayton01 <matthew.clayton1216@gmail.com>
2022-07-24 16:06:08 +0100

    fixed admin panel

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/admin');
 
     eleventyConfig.addFilter("postDate", (dateobj) => {
         return DateTime.fromJSDate(dateobj).toLocaleString(DateTime.DATE_MED)
